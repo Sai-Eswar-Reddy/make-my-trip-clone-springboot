0b66c7f011e1fcf44ee347a5561f2970b3995a3d
Sai-Eswar-Reddy <saieswarreddyg@gmail.com>
2026-04-25 16:19:41 +0530

    added hotel controller

---
+++ src/main/java/com/makemytrip/makemytrip/controllers/HotelController.java
@@ -0,0 +1,22 @@
+package com.makemytrip.makemytrip.controllers;
+
+import com.makemytrip.makemytrip.models.Hotel;
+import com.makemytrip.makemytrip.repositories.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hotel")
+@CrossOrigin(origins = "*")
+public class HotelController {
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    @GetMapping
+    public List<Hotel> getHotels() {
+        return hotelRepository.findAll();
+    }
+}
